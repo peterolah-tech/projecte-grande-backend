4f25575ae6939adfa529cda1e0516f5d93a2367a
Ani <jancso.anita@gmail.com>
2021-09-19 12:01:26 +0200

    Create spring boot project

---
+++ src/main/java/com/codecool/projectegrandebackend/ProjecteGrandeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.projectegrandebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecteGrandeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecteGrandeBackendApplication.class, args);
+    }
+
+}
