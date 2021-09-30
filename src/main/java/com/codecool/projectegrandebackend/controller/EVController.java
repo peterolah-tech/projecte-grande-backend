2a62894ec4100b2f6e07c81313df377448f307fb
Ani <jancso.anita@gmail.com>
2021-09-30 13:06:07 +0200

    Rest api call with ParameterizedTypeReference

--- src/main/java/com/codecool/projectegrandebackend/controller/EVController.java
+++ src/main/java/com/codecool/projectegrandebackend/controller/EVController.java
@@ -1,15 +1,13 @@
 package com.codecool.projectegrandebackend.controller;
 
 import com.codecool.projectegrandebackend.model.EV;
-import com.codecool.projectegrandebackend.model.evgenerated.EVResponseItem;
 import com.codecool.projectegrandebackend.service.EVService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 public class EVController {
@@ -17,8 +15,8 @@ public class EVController {
     @Autowired
     private EVService evService;
 
-    @GetMapping("api/ev")
-    public ArrayList getEV(){
+    @GetMapping("api/v1/ev")
+    public Set<EV> getEV(){
         System.out.println(evService.getEVData());
         return evService.getEVData();
     }
