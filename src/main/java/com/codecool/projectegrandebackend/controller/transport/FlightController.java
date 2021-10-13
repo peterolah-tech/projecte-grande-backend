8753b1d755e65c7e110fcc416eb1b25b8cf5c433
nemviktor <wookie7373@gmail.com>
2021-10-13 18:29:47 +0200

    Committing changes before checkout.

--- src/main/java/com/codecool/projectegrandebackend/controller/transport/FlightController.java
+++ src/main/java/com/codecool/projectegrandebackend/controller/transport/FlightController.java
@@ -1,5 +1,6 @@
 package com.codecool.projectegrandebackend.controller.transport;
 
+import com.codecool.projectegrandebackend.model.generated.transport.flight.FlightTransport;
 import com.codecool.projectegrandebackend.model.generated.transport.flight.flightPostInput_generated.FlightPostInput;
 import com.codecool.projectegrandebackend.repository.AirportRepository;
 import com.codecool.projectegrandebackend.service.transport.FlightService;
@@ -9,6 +10,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("api/v1")
 @RestController
@@ -32,6 +35,10 @@ public class FlightController {
     public String getFlightTransport(@RequestBody FlightPostInput inputData) {
         Gson g = new Gson();
         String jsonString = g.toJson(inputData);
+//        FlightTransport flightTransport= flightService.getFlightData(jsonString);
+//        Map<String, String> responseCarbonData = new HashMap<>();
+//        responseCarbonData.put("distance_flew", flightTransport.);
+//        responseCarbonData.put("consumed_carbon", flightTransport.getEquivalentCarbonInKg());
         return flightService.getFlightData(jsonString).getEquivalentCarbonInKg();
     }
 }
