582e330a239a8c275453ab034b4dd0c640752cd0
peterolah-tech <peter.olah.hu@gmail.com>
2021-10-15 22:19:45 +0200

    Start handling meal additions by users

--- src/main/java/com/codecool/projectegrandebackend/controller/recipes/RecipesController.java
+++ src/main/java/com/codecool/projectegrandebackend/controller/recipes/RecipesController.java
@@ -3,9 +3,11 @@ package com.codecool.projectegrandebackend.controller.recipes;
 import com.codecool.projectegrandebackend.model.generated.recipes.Recipes;
 import com.codecool.projectegrandebackend.service.recipes.RecipesApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RecipesController {
@@ -16,6 +18,23 @@ public class RecipesController {
     @GetMapping("api/v1/recipes/{cuisine}/{diet}")
     public Recipes getRecipesByCuisineAndDiet(@PathVariable("cuisine") String cuisine, @PathVariable("diet") String diet) {
         return recipesApiService.getRecipesByCuisineAndDiet(cuisine, diet);
+    }
+
+    @RequestMapping(value="/food/api/v1/add-meal",
+            method= RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createRole(@RequestBody Map<String, String> formData){
+
+
+
+//        AppUser newUser = AppUser.builder()
+//                .username(formData.get("name"))
+//                .email(formData.get("email"))
+//                .password(passwordEncoder.encode(formData.get("password")))
+//                .roles(List.of("ROLE_USER"))
+//                .build();
+//
+//        userRepository.save(newUser);
     }
 
 }
